39ead9fa934d79941d91e231cc86146c010ff151
Magno F. Nóbrega <magnofnobrega@outlook.com>
2023-02-15 00:46:12 -0300

    while

---
+++ eclipse-workspace/sintaxe-variaveis-e-fluxo/src/TestaWhile.java
@@ -0,0 +1,13 @@
+
+public class TestaWhile {
+	public static void main(String[] args) {
+		// imprimindo de 0 a 10
+		int contador = 0;
+		while (contador <= 10) {
+			System.out.println(contador);
+			// contador = contador + 1
+			// contador += 1
+			contador++;
+		}
+	}
+}
